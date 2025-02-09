642b4f75771463076cbb6b6422683316403a844d
Duc Nguyen <minhduc140583@gmail.com>
2025-02-09 23:52:17 +0700

    Refactor code

--- internal/role/adapter.go
+++ internal/role/adapter.go
@@ -5,11 +5,10 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	q "github.com/core-go/sql"
 	"reflect"
 	"strconv"
 	"strings"
-
-	q "github.com/core-go/sql"
 )
 
 const ActionNone int32 = 0
@@ -98,55 +97,67 @@ func (s *RoleAdapter) Load(ctx context.Context, roleId string) (*Role, error) {
 	return &role, nil
 }
 
-func (s *RoleAdapter) Create(ctx context.Context, role *Role) (int64, error) {
-	sts, err := buildInsertStatements(role, s.Driver, s.BuildParam, s.Schema, s.ModuleSchema)
-	if err != nil {
-		return 0, err
+func buildModules(roleId string, privileges []string) ([]roleModule, error) {
+	if privileges == nil || len(privileges) <= 0 {
+		return nil, nil
 	}
-	return sts.Exec(ctx, s.db)
+	modules := make([]roleModule, 0)
+	for _, p := range privileges {
+		m := toModules(p)
+		m.RoleId = roleId
+		modules = append(modules, m)
+	}
+	return modules, nil
 }
-func buildInsertStatements(role *Role, driver string, buildParam func(int) string, roleSchema *q.Schema, roleModuleSchema *q.Schema) (q.Statements, error) {
+func toModules(menu string) roleModule {
+	s := strings.Split(menu, " ")
+	permission := ActionNone
+	if len(s) >= 2 {
+		i, err := strconv.ParseInt(s[1], 16, 64)
+		if err == nil {
+			permission = int32(i)
+		}
+	}
+	p := roleModule{ModuleId: s[0], Permissions: permission}
+	return p
+}
+func (s *RoleAdapter) Create(ctx context.Context, role *Role) (int64, error) {
 	modules, er1 := buildModules(role.RoleId, role.Privileges)
 	if er1 != nil {
-		return nil, er1
+		return -1, er1
 	}
 	sts := q.NewStatements(true)
-	sts.Add(q.BuildToInsert("roles", role, buildParam, roleSchema))
+	sts.Add(q.BuildToInsert("roles", role, s.BuildParam, s.Schema))
 	if modules != nil {
-		query, args, er2 := q.BuildToInsertBatch("roleModules", modules, driver, roleModuleSchema)
+		query, args, er2 := q.BuildToInsertBatch("roleModules", modules, s.Driver, s.ModuleSchema)
 		if er2 != nil {
-			return nil, er2
+			return -1, er2
 		}
 		sts.Add(query, args)
 	}
-	return sts, nil
+
+	return sts.Exec(ctx, s.db)
 }
 
 func (s *RoleAdapter) Update(ctx context.Context, role *Role) (int64, error) {
-	sts, err := buildUpdateStatements(role, s.Driver, s.BuildParam, s.Schema, s.ModuleSchema)
-	if err != nil {
-		return 0, err
-	}
-	return sts.Exec(ctx, s.db)
-}
-func buildUpdateStatements(role *Role, driver string, buildParam func(int) string, roleSchema *q.Schema, roleModuleSchema *q.Schema) (q.Statements, error) {
 	modules, err := buildModules(role.RoleId, role.Privileges)
 	if err != nil {
-		return nil, err
+		return -1, err
 	}
 	sts := q.NewStatements(true)
-	sts.Add(q.BuildToUpdate("roles", role, buildParam, roleSchema))
+	sts.Add(q.BuildToUpdate("roles", role, s.BuildParam, s.Schema))
 
-	deleteModules := fmt.Sprintf("delete from roleModules where roleId = %s", buildParam(1))
+	deleteModules := fmt.Sprintf("delete from roleModules where roleId = %s", s.BuildParam(1))
 	sts.Add(deleteModules, []interface{}{role.RoleId})
 	if modules != nil {
-		query, args, er2 := q.BuildToInsertBatch("roleModules", modules, driver, roleModuleSchema)
+		query, args, er2 := q.BuildToInsertBatch("roleModules", modules, s.Driver, s.ModuleSchema)
 		if er2 != nil {
-			return nil, er2
+			return -1, er2
 		}
 		sts.Add(query, args)
 	}
-	return sts, nil
+
+	return sts.Exec(ctx, s.db)
 }
 
 func (s *RoleAdapter) Patch(ctx context.Context, role map[string]interface{}) (int64, error) {
@@ -197,10 +208,14 @@ func (s *RoleAdapter) Delete(ctx context.Context, id string) (int64, error) {
 			return -1, er0
 		}
 	}
-	sts, er1 := buildDeleteStatements(id, s.BuildParam)
-	if er1 != nil {
-		return 0, er1
-	}
+	sts := q.NewStatements(false)
+
+	deleteModules := fmt.Sprintf("delete from roleModules where roleId = %s", s.BuildParam(1))
+	sts.Add(deleteModules, []interface{}{id})
+
+	deleteRole := fmt.Sprintf("delete from roles where roleId = %s", s.BuildParam(1))
+	sts.Add(deleteRole, []interface{}{id})
+
 	return sts.Exec(ctx, s.db)
 }
 func checkExist(db *sql.DB, sql string, args ...interface{}) (bool, error) {
@@ -214,23 +229,24 @@ func checkExist(db *sql.DB, sql string, args ...interface{}) (bool, error) {
 	}
 	return false, nil
 }
-func buildDeleteStatements(roleId string, buildParam func(int) string) (q.Statements, error) {
-	sts := q.NewStatements(false)
-
-	deleteModules := fmt.Sprintf("delete from roleModules where roleId = %s", buildParam(1))
-	sts.Add(deleteModules, []interface{}{roleId})
-
-	deleteRole := fmt.Sprintf("delete from roles where roleId = %s", buildParam(1))
-	sts.Add(deleteRole, []interface{}{roleId})
-
-	return sts, nil
-}
 
 func (s *RoleAdapter) AssignRole(ctx context.Context, roleId string, users []string) (int64, error) {
-	sts, err := buildAssignRoleStatements(roleId, users, s.Driver, s.BuildParam, s.UserSchema)
+	modules, err := buildRoleUser(roleId, users)
 	if err != nil {
-		return 0, err
+		return -1, err
 	}
+	sts := q.NewStatements(true)
+
+	deleteModules := fmt.Sprintf("delete from userroles where roleId = %s", s.BuildParam(1))
+	sts.Add(deleteModules, []interface{}{roleId})
+	if modules != nil {
+		query, args, er2 := q.BuildToInsertBatch("userRoles", modules, s.Driver, s.UserSchema)
+		if er2 != nil {
+			return -1, er2
+		}
+		sts.Add(query, args)
+	}
+
 	return sts.Exec(ctx, s.db)
 }
 func buildRoleUser(roleId string, users []string) ([]userRole, error) {
@@ -242,47 +258,4 @@ func buildRoleUser(roleId string, users []string) ([]userRole, error) {
 		modules = append(modules, userRole{UserId: u, RoleId: roleId})
 	}
 	return modules, nil
-}
-func buildAssignRoleStatements(roleId string, users []string, driver string, buildParam func(int) string, userRoleSchema *q.Schema) (q.Statements, error) {
-	modules, err := buildRoleUser(roleId, users)
-	if err != nil {
-		return nil, err
-	}
-	sts := q.NewStatements(true)
-
-	deleteModules := fmt.Sprintf("delete from userroles where roleId = %s", buildParam(1))
-	sts.Add(deleteModules, []interface{}{roleId})
-	if modules != nil {
-		query, args, er2 := q.BuildToInsertBatch("userRoles", modules, driver, userRoleSchema)
-		if er2 != nil {
-			return nil, er2
-		}
-		sts.Add(query, args)
-	}
-	return sts, nil
-}
-
-func buildModules(roleId string, privileges []string) ([]roleModule, error) {
-	if privileges == nil || len(privileges) <= 0 {
-		return nil, nil
-	}
-	modules := make([]roleModule, 0)
-	for _, p := range privileges {
-		m := toModules(p)
-		m.RoleId = roleId
-		modules = append(modules, m)
-	}
-	return modules, nil
-}
-func toModules(menu string) roleModule {
-	s := strings.Split(menu, " ")
-	permission := ActionNone
-	if len(s) >= 2 {
-		i, err := strconv.ParseInt(s[1], 16, 64)
-		if err == nil {
-			permission = int32(i)
-		}
-	}
-	p := roleModule{ModuleId: s[0], Permissions: permission}
-	return p
 }
